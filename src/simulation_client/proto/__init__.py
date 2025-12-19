27cdf166ea59ccff1236e44eb2ded31e2ec6a11f
Sergey <gerasimovsergey2004@gmail.com>
2025-12-19 13:00:17 +0300

    fix: fix import, remove from .gitignore proto

---
+++ src/simulation_client/proto/__init__.py
@@ -0,0 +1,12 @@
+"""
+Generated protobuf and gRPC code.
+This module is auto-generated from simulator.proto file.
+"""
+
+from . import simulator_pb2
+from . import simulator_pb2_grpc
+
+__all__ = [
+    "simulator_pb2",
+    "simulator_pb2_grpc",
+]
